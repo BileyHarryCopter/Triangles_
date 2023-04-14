6aa3b1dc73ce4a847197069698ab9b304aef7dc4
BileyHarryCopter <gerasimenko.dv@phystech.edu>
2023-04-14 16:26:10 +0300

    Added model with vertex buffer

---
+++ include/vulkan/model.hpp
@@ -0,0 +1,49 @@
+#pragma once
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/glm.hpp>
+
+#include "device.hpp"
+
+namespace VKModel
+{
+
+struct Vertex
+{
+    glm::vec2 position;
+
+    static std::vector<VkVertexInputBindingDescription>     get_binding_descriptions();
+    static std::vector<VkVertexInputAttributeDescription> get_attribute_descriptions();
+};
+
+class Model final
+{
+    VKDevice::Device&                        device_;
+    VkBuffer            vertexbuff_ = VK_NULL_HANDLE;
+    VkDeviceMemory   vertexbuffmem_ = VK_NULL_HANDLE;
+    uint32_t           vertexcount_ =              0;
+
+public:
+
+    Model (VKDevice::Device& device, const std::vector<Vertex>& vertices);
+    ~Model();
+
+    Model(const Model &rhs) = delete;
+    Model &operator=(const Model& rhs) = delete;
+
+    void bind(VkCommandBuffer commandbuffer);
+    void draw(VkCommandBuffer commandbuffer);
+
+private:
+    void createVertexBuffer(const std::vector<Vertex>& vertices);
+};
+
+void createBuffer(VKDevice::Device& device, VkDeviceSize size,
+                  VkBufferUsageFlags usage, VkMemoryPropertyFlags properties,
+                  VkBuffer &buffer, VkDeviceMemory &bufferMemory);
+
+}   //  end of the VKModel namespace
---
+++ src/src/model.cpp
@@ -0,0 +1,67 @@
+#include "model.hpp"
+
+#include <cassert>
+#include <cstring>
+
+namespace VKModel
+{
+
+    Model::Model (VKDevice::Device& device, const std::vector<Vertex>& vertices) :
+                  device_{device}
+    {
+        createVertexBuffer(vertices);
+    }
+
+    Model::~Model()
+    {
+        vkDestroyBuffer(device_.get_logic(),    vertexbuff_, nullptr);
+        vkFreeMemory   (device_.get_logic(), vertexbuffmem_, nullptr);
+    }
+
+    void Model::createVertexBuffer(const std::vector<Vertex>& vertices)
+    {
+        vertexcount_ = static_cast<uint32_t>(vertices.size());
+        assert(vertexcount_ >= 3 && "Vertex count must be at least 3\n");
+
+        VkDeviceSize buffsize = sizeof(vertices[0]) * vertexcount_;
+
+        createBuffer(device_, buffsize, VK_BUFFER_USAGE_VERTEX_BUFFER_BIT, 
+                     VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
+                     vertexbuff_, vertexbuffmem_);
+
+        void *mapping_data = nullptr;
+        vkMapMemory  (device_.get_logic(), vertexbuffmem_, 0, buffsize, 0, &mapping_data);
+        memcpy       (mapping_data, vertices.data(), static_cast<size_t>(buffsize));
+        vkUnmapMemory(device_.get_logic(), vertexbuffmem_);
+    }
+
+    void Model::draw(VkCommandBuffer commandbuffer)
+    {
+        vkCmdDraw(commandbuffer, vertexcount_, 1, 0, 0);    //  put here some constants
+    }
+
+    void Model::bind(VkCommandBuffer commandbuffer)
+    {
+        VkBuffer buffers[] = {vertexbuff_};
+        VkDeviceSize offsets[] = {0};
+        vkCmdBindVertexBuffers(commandbuffer, 0, 1, buffers, offsets);  //  and here too
+    }
+
+    std::vector<VkVertexInputBindingDescription>     Vertex::get_binding_descriptions()
+    {
+        std::vector<VkVertexInputBindingDescription> bindingDescriptions{};
+
+        bindingDescriptions.push_back({0, sizeof(Vertex), VK_VERTEX_INPUT_RATE_VERTEX});
+
+        return bindingDescriptions;
+    }
+    std::vector<VkVertexInputAttributeDescription> Vertex::get_attribute_descriptions()
+    {
+        std::vector<VkVertexInputAttributeDescription> attributeDescriptions{};
+
+        attributeDescriptions.push_back({0, 0, VK_FORMAT_R32G32B32_SFLOAT, 0});
+
+        return attributeDescriptions;
+    }
+
+}   //  end of the VKModel namespace
